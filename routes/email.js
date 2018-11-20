d85cac43e55589867079bc335f8da687793632d2
xiekun <xiekun@f3dt.com>
2018-11-20 17:21:27 +0800

    添加cheerio，增加特定邮箱的邮件获取和返回数据的限制

--- routes/email.js
+++ routes/email.js
@@ -7,6 +7,9 @@ const config = JSON.parse(fs.readFileSync('./config.json'));
 
 const Imap = require('imap'),
       inspect = require('util').inspect;
+const { simpleParser } = require('mailparser');
+
+const cheerio = require('cheerio');
 
 // receive
 const imap = new Imap({
@@ -34,7 +37,114 @@ let transporter = nodemailer.createTransport({
 // });
 // imap.connect();
 
+/**
+ * query mail in specific mailbox
+ * query: ?mailbox=mailbox
+ */
 router.get('/', function (req, res, next) {
+  const mailbox = req.query.mailbox;
+  if (mailbox) {
+    imap.once('ready', () => {
+      imap.openBox(mailbox, true, (err, box) => {
+        if (err) {
+          res.statusCode = 500;
+          res.json({msg: err.message});
+          imap.end();
+        } else {
+          imap.search(['All', ['SINCE', new Date('2018-011-01')]], (err, results) => {
+            if (err) {
+              res.statusCode = 500;
+              res.json({msg: err.message});
+              imap.end();
+            } else {
+              let f = imap.fetch(results, {bodies: ''});
+              let msgs = [];
+              let promises = [];
+              f.on('message', (msg, seqno) => {
+                console.log('Message #%d', seqno);
+                let prefix = `(#${seqno}) `;
+                msg.on('body', (stream, info) => {
+                  let promise = (function () {
+                    return new Promise((resolve, reject) => {
+                      simpleParser(stream, (err2, mail) => {
+                        if (err2) {
+                          console.log('Read mail executor error .....', err2);
+                          reject();
+                          return ;
+                        }
+                        // mail will have everything, create meaningful data from it.
+                        // const fileName = `msg-${seqno}-body.txt`;
+                        // const fullFilePath = path.join('<path to store>', dir, fileName);
+                        const emailEnvolope = {};
+                        emailEnvolope.from = mail.from.text;
+                        emailEnvolope.date = mail.date;
+                        emailEnvolope.to = mail.to.text;
+                        emailEnvolope.subject = mail.subject;
+                        // emailEnvolope.text = mail.text && mail.text.replace(/(\[(https|http):\/\/[\s\S]+?\])*/g, '').replace(/[\s]+/g, ' ').trim().slice(0, 30).replace(/(\n)+/g, ' ');
+                        // emailEnvolope.html = mail.html;
+                        emailEnvolope.attachments = [];
+                        if (mail.html) {
+                          let $ = cheerio.load(mail.html);
+                          emailEnvolope.overview = $.text();
+                        } else {
+                          emailEnvolope.overview = mail.text && mail.text.replace(/(\[(https|http):\/\/[\s\S]+?\])*/g, '').replace(/[\s]+/g, ' ');  
+                        }
+                        emailEnvolope.overview = emailEnvolope.overview.trim().replace(/(\s)+/g, ' ').slice(0, 50);
+    
+                        // write attachments
+                        for (let i = 0; i < mail.attachments.length; i += 1) {
+                            const attachment = mail.attachments[i];
+                            const { filename } = attachment;
+                            emailEnvolope.attachments.push(filename);
+                          //  fs.writeFileSync(path.join('<path to store>', dir, filename), attachment.content, 'base64'); // take encoding from attachment ?
+                        }
+                        // const contents = JSON.stringify(emailEnvolope);
+                        // fs.writeFileSync(fullFilePath, contents);
+                        // body = contents;
+                        // console.log(body);
+                        msgs.push(emailEnvolope);
+                        resolve();
+                        console.log('processing mail done....');
+                      });
+                      
+                    });
+                  })();
+                  promises.push(promise);
+                });
+                msg.once('attributes', attrs => {
+                  console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+                });
+                msg.once('end', () => {
+                  console.log(prefix, 'Finished');
+                  
+                });
+              });
+              f.once('error', err => {
+                console.log('Fetch error: ' + err);
+                res.statusCode = 500;
+                res.json({msg: err.message});
+              });
+              f.once('end', () => {
+                console.log('Done fetching all messages!');
+                Promise.all(promises).then(() => {
+                  res.json({msg: msgs});
+                });
+                imap.end();
+              });
+            }
+          });
+        }
+      });
+    });
+    imap.once('error', err => {
+      res.statusCode = 500;
+      res.json({msg: err.message});
+    });
+    imap.connect();
+  } else {
+    res.statusCode = 400;
+    res.json({msg: 'mailbox should not be empty'});
+  }
   // imap.once('ready', function () {
   //   imap.openBox('INBOX', true, function (err, box) {
   //     if (err) throw err;
@@ -112,14 +222,27 @@ router.get('/:id', function (req, res, next) {
   
 });
 router.post('/', function (req, res, next) {
-  let mailOptions = {
-    from: `Fred Foo <${config.user}>`, // sender address
-    to: 'xiekun@f3dt.com', // list of receivers
-    cc: 'xiekun@f3dt.com',
-    bcc: 'xiekun@f3dt.com',
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world? this is a test for nodemailer</b>' // html body
+  let { from, to, cc, bcc, subject, text, html } = req.body;
+  if (from && to) {
+    let mailOptions = {
+      from, // sender address
+      to, // list of receivers
+      cc,
+      bcc,
+      subject, // Subject line
+      text, // plain text body
+      html // html body
+    };
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        res.json({name: 'fail'});
+        return console.log(err);
+      }
+      console.log('Message sent: %s', info.messageId);
+      // Preview only available when sending through an Ethereal account
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      res.json({name: 'ok', accepted: info.accepted, rejected: info.rejected});
+    });
   }
   // transporter.verify(function (error, success) {
   //   if (error) {
@@ -130,16 +253,6 @@ router.post('/', function (req, res, next) {
   //     res.json({name: 'ok'});
   //   }
   // });
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      res.json({name: 'fail'});
-      return console.log(err);
-    }
-    console.log('Message sent: %s', info.messageId);
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    res.json({name: 'ok', accepted: info.accepted, rejected: info.rejected});
-  });
   
 });
 router.put('/', function (req, res, next) {
