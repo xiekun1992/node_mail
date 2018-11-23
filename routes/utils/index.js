69c7608a0e75c6bd18ae704fdf86a986d7d8cc02
xiekun <xiekun@f3dt.com>
2018-11-23 17:25:32 +0800

    添加辅助方法

--- routes/utils/index.js
+++ routes/utils/index.js
@@ -1,8 +1,8 @@
-let resolvePath = function () {
+let resolvePath = () => {
   let args = Array.prototype.slice.call(arguments);
   return args.filter(arg => arg).join('/');
 }
-let recurseBox = function (box) {
+let recurseBox = (box) => {
   let arr = [];
   for (let e in box) {
     if (box.hasOwnProperty(e)) {
@@ -17,8 +17,33 @@ let recurseBox = function (box) {
   }
   return arr;
 }
+let hasAttachments = (obj) => {
+  if (Object.prototype.toString.call(obj) === '[object Object]') {
+    if (obj.disposition && obj.disposition.type == 'attachment') {
+      return true;
+    }
+  } else if (Object.prototype.toString.call(obj) === '[object Array]') {
+    for (let o of obj) {
+      if (hasAttachments(o)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+let combineMailbox = (list) => {
+  if (list) {
+    return list.map(f => ({
+      name: f.name && f.name || `${f.mailbox}@${f.host}`,
+      host: `${f.mailbox}@${f.host}`
+    }));
+  }
+  return list;
+}
 
 module.exports = {
   resolvePath,
-  recurseBox
+  recurseBox,
+  hasAttachments,
+  combineMailbox
 }
